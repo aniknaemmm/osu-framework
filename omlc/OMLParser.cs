ea40690fb7d66d5457b5d28ae692d40453ca4924
Drew DeVault <sir@cmpwn.com>
2016-10-03 16:08:26 -0400

    Initial parser layout

---
+++ omlc/OMLParser.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using YamlDotNet;
+using omlc.Tree;
+using YamlDotNet.RepresentationModel;
+
+namespace omlc
+{
+    public static class OMLParser
+    {
+        public static OObject Parse(StreamReader stream)
+        {
+            var result = new OObject();
+            var yaml = new YamlStream();
+            yaml.Load(stream);
+            var root = (YamlMappingNode)yaml.Documents[0].RootNode;
+            System.Console.WriteLine(root);
+            return result;
+        }
+    }
+}
--- omlc/Program.cs
+++ omlc/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.IO;
 
 namespace omlc
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var input = new StreamReader(args[0]))
+            using (var output = new StreamWriter(args[1]))
+            {
+                var oobject = OMLParser.Parse(input);
+                // TODO: do something with that
+            }
+            return 0;
         }
     }
 }
